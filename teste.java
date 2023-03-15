e00fbb4c87cb678c0b09e3b1429c3bc9772d4524
DaniloLMD <danilolmd04@gmail.com>
2023-03-15 14:31:14 -0300

    ata

--- teste.java
+++ teste.java
@@ -1,20 +1,5 @@
 public class teste {
-
-    static int x = 0;
-    static int y = 0;
-
-
-    public static void main(String[] args) {
-            teste comando = new teste();
-            System.out.println(x + " " + y);
-            int teste = comando.Soma(5, 3);
-            System.out.println(teste);
-            System.out.printf("%d %d", x, y);
-        }
-
-    int Soma(int x, int y){
-            this.x = x;
-            this.y = y;
-            return x + y;
+    public static void main(String[] x){
+        System.out.println("Oi");
         }
     }
